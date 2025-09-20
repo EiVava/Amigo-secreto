9c2cd8f98fe569597404fc2bb1e79abb34a22a65
wagner de oliveira <wagnerdeoliveira432@gmail.com>
2025-09-20 12:54:17 -0300

    adicionado js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigosX = [];
+
+
+function adicionarAmigo() {
+    if (document.getElementById("amigo").value === "") {
+        alert("Por favor, insira o nome do amigo.");}
+    else {
+        amigosX.push(document.getElementById("amigo").value);
+        let novoItemLista = document.createElement("li");
+        novoItemLista.textContent = document.getElementById("amigo").value;
+        document.getElementById("listaAmigos").appendChild(novoItemLista);
+        document.getElementById("amigo").value = "";
+    }
+}
+
+function sortearAmigo() {
+    if (amigosX.length === 0) {
+        alert("Nenhum amigo adicionado para sortear.");
+    } else {
+        let indiceSorteado = Math.floor(Math.random() * amigosX.length);
+        console.log(indiceSorteado);
+        let amigoSorteado = amigosX[indiceSorteado];
+        document.getElementById("resultado").textContent = "O amigo sorteado foi: " + amigoSorteado;
+    }
+}
+
+function reiniciarSorteio() {
+    amigosX = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").textContent = "";
+}
